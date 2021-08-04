7927db2b7d2fd34a5ef86a8d1fec390668ad3158
Benson Li <81859388+Benson2121@users.noreply.github.com>
2021-08-04 13:33:00 +0800

    Create Next bigger number with the same digits.py

---
+++ Next bigger number with the same digits.py
@@ -0,0 +1,20 @@
+def next_bigger(n):
+    list1=list(str(n))
+    list2=[list1[-1]]
+    if sorted(list1,reverse=True)==list1:
+        return -1
+    
+    for i in range(1,len(list1)):
+        list2.append(list1[-i-1])
+        list2.sort()
+        if list1[-i]>list1[-i-1]:
+            k=min(j for j in list2 if j>list1[-i-1])
+            list1[-i-1]=k
+            list2.remove(k)
+            list1=list1[:-i]
+            list1=list1+list2
+            break
+        
+    if list1[0]=='0' or int(''.join(list1))==n:
+        return -1
+    return int(''.join(list1))
